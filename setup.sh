1650cab8e5c938e310000b90266ee77056faaf6a
Roger Reeuwijk <rodgerabbit@gmail.com>
2025-12-17 21:58:14 +0100

    feat: Add UID and GID environment variables to setup and update scripts for improved user context management

--- setup.sh
+++ setup.sh
@@ -12,6 +12,10 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+# UID and Group ID variables
+export UID=$(id -u)
+export GID=$(id -g)
+
 # Helper functions
 print_success() {
     echo -e "${GREEN}✓${NC} $1"
