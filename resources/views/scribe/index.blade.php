6ada9d5c9f51769815aed587461654f9cd382284
Roger Reeuwijk <rodgerabbit@gmail.com>
2026-02-14 11:04:12 +0100

    chore: Update composer.lock and package-lock.json to latest versions of Inertia.js and Nette packages for improved stability and performance; adjust API documentation examples for player winner status

--- resources/views/scribe/index.blade.php
+++ resources/views/scribe/index.blade.php
@@ -187,7 +187,7 @@
     </ul>
 
     <ul class="toc-footer" id="last-updated">
-        <li>Last updated: February 11, 2026</li>
+        <li>Last updated: February 14, 2026</li>
     </ul>
 </div>
 
@@ -2594,7 +2594,7 @@ Must be one of:
             <code>false</code>
         </label>
     <br>
-<p>Example: <code>true</code></p>
+<p>Example: <code>false</code></p>
                     </div>
                                                                 <div style="margin-left: 14px; clear: unset;">
                         <b style="line-height: 2;"><code>position</code></b>&nbsp;&nbsp;
